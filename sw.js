8ff1bc57da2343b2c5d6edb3c227a3634b7bdd35
olloberdi1123 <138999801+olloberdi1123@users.noreply.github.com>
2026-02-24 10:23:48 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'keeng-saas-cache-v1';
+
+// Oflayn ishlashi uchun xotiraga olinadigan fayllar ro'yxati
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
+    'https://telegram.org/js/telegram-web-app.js',
+    'https://unpkg.com/html5-qrcode',
+    'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js',
+    'https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// O'rnatish va xotiraga yuklash
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Fayllar oflayn ishlash uchun keshlandi');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Eski keshni tozalash
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Internet yo'q bo'lsa xotiradan (oflayn) ochish
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        }).catch(() => {
+            console.log("Internet yo'q, xotirada ham topilmadi.");
+        })
+    );
+});
